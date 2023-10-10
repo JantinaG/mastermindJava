4c0c75722278a16b96f95c10f2debd2397512ed3
Jantina Gerike <jantina.gerike@gmail.com>
2023-10-10 13:15:21 +0200

    Initial set up

---
+++ src/mastermindJava/Application.java
@@ -0,0 +1,10 @@
+package mastermindJava;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
